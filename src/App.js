7e554c195ad8d2105edfedfa22c002ea40727414
koushiksaha16021996 <koushiksaha16021996>
2021-04-17 16:01:24 +0530

    redux_thunk_appltmiddlewire

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {doIncrement, doDecrement} from './Redux'
 import Apps from './Apps'
 import { Provider } from 'react-redux';
-import {store} from './Reduxtoolkit'
+import {showTodos} from './Redux'
+
 
 const App =()=>{
   const state=useSelector((state)=>state)
-  const {counter} = state
+  
   const usedispatch=useDispatch();
+  useEffect(()=>{
+    usedispatch(showTodos());
+  },[])
+ // const counter=state.count.counter
+  const {todo} = state;
+  console.log(todo,"app.js")
+  
   return(
-    <div className="container">
-      <fieldset className="my-fields">
-        <legend>Using Redux</legend>
-        <h1>Counter: {counter}</h1>
-        <button onClick={()=>usedispatch(doIncrement(1))}>Increment</button>
-        <button onClick={()=>usedispatch(doDecrement(1))}>Decrement</button>
-      </fieldset>
-      <Provider store={store}>
-        <Apps/>
-      </Provider>
-
+    //<div className="container">
+    //  <fieldset className="my-fields">
+    //    <legend>Using Redux</legend>
+    //    <h1>Counter: {counter}</h1>
+    //    <button onClick={()=>usedispatch(doIncrement(1))}>Increment</button>
+    //    <button onClick={()=>usedispatch(doDecrement(1))}>Decrement</button>
+    //  </fieldset>
+    //  
+    //  <div>
+    //    {todo.map(item=><div>
+    //      <h1>{item.id}</h1>
+    //      <h1>{item.title}</h1>
+    //    </div>)}
+    //  </div>
+    //</div>
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <td>UserId</td>
+            <td>Id</td>
+            <td>Title</td>
+          </tr>
+        </thead>
+        <tbody>
+          {todo?.map(item=>{
+            return(
+              <tr>
+                <td>{item?.userId}</td>
+                <td>{item?.id}</td>
+                <td>{item?.title}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
--- src/Redux.js
+++ src/Redux.js
@@ -1,48 +1,81 @@
-import { createStore } from "redux"
+import axios from "axios"
+import { applyMiddleware, combineReducers, createStore } from "redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import thunk from "redux-thunk"
 
 console.log("----redux----")
-const initialState = { counter: 0 }
+//const initialState = { counter: 0 }
 
-const reducer=(state=initialState,action)=>{
+//const counterReducer=(state=initialState,action)=>{
+//    switch(action.type){
+//        case "INCREMENT" : return{
+//            counter : state.counter +action.payload
+//        }
+//        case "DECREMENT" : return{
+//            counter : state.counter -action.payload
+//        }
+//        default : return state
+//    }
+//}
+const todoReducer=(state={
+   todo: []
+},action)=>{
     switch(action.type){
-        case "INCREMENT" : return{
-            counter : state.counter +action.payload
-        }
-        case "DECREMENT" : return{
-            counter : state.counter -action.payload
-        }
+        case "SHOW": return {todo: action.payload}
         default : return state
     }
 }
+//const rootReducer=combineReducers({
+//    count: counterReducer,
+//    todos: todoReducer})
+export const store=createStore(todoReducer , composeWithDevTools(applyMiddleware(thunk)))
 
-export const store=createStore(reducer)
+export const getTodos=(payload)=>{
+    return{
+        type: "SHOW",
+        payload
+    }
+}
+
+export const showTodos=()=>{
+    return(dispatch)=>{
+        axios.get("https://jsonplaceholder.typicode.com/todos").then(res=>{
+            dispatch(getTodos(res.data))
+            console.log(res.data)
+        })
+        
+    }
+}
+
+//store.dispatch(showTodos())
+
 //console.log(store.getState())
-const increment={
-    type:"INCREMENT"
-}
-const decrement={
-    type:"DECREMENT"
-}
+//const increment={
+//    type:"INCREMENT"
+//}
+//const decrement={
+//    type:"DECREMENT"
+//}
 //store.dispatch(increment)
 //console.log(store.getState())
 //store.dispatch(decrement)
-console.log(store.getState(),"initial state")
-store.subscribe(()=>console.log(store.getState()))
-const INCREMENT="INCREMENT"
-const DECREMENT="DECREMENT"
-export const doIncrement=(value)=>{
-    return{
-        type: INCREMENT,
-        payload: value
-    }
-}
-
-export const doDecrement=(value)=>{
-    return{
-        type: DECREMENT,
-        payload: value
-    }
-}
+//console.log(store.getState(),"initial state")
+//store.subscribe(()=>console.log(store.getState()))
+//const INCREMENT="INCREMENT"
+//const DECREMENT="DECREMENT"
+//export const doIncrement=(value)=>{
+//    return{
+//        type: INCREMENT,
+//        payload: value
+//    }
+//}
+//
+//export const doDecrement=(value)=>{
+//    return{
+//        type: DECREMENT,
+//        payload: value
+//    }
+//}
 
 //store.dispatch(doIncrement(1))
 //store.dispatch(doIncrement(10))
